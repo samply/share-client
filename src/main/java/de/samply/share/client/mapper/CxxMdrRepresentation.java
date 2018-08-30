543d77f40bf4cae7951414e267e8853763a1cdfb
juarez <d.juarez@dkfz-heidelberg.de>
2018-08-30 09:26:41 +0200

    Cxx Mapper

---
+++ src/main/java/de/samply/share/client/mapper/CxxMdrRepresentation.java
@@ -0,0 +1,75 @@
+package main.java.de.samply.share.client.mapper;/*
+ * Copyright (C) 2018 Medizinische Informatik in der Translationalen Onkologie,
+ * Deutsches Krebsforschungszentrum in Heidelberg
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with Jersey (https://jersey.java.net) (or a modified version of that
+ * library), containing parts covered by the terms of the General Public
+ * License, version 2.0, the licensors of this Program grant you additional
+ * permission to convey the resulting work.
+ */
+
+public class CxxMdrRepresentation {
+
+    Integer oid;
+    String entitySource = "CENTRAXX";
+    Integer creator = 1;
+    Integer mdrMappingOid;
+    String mdrPermittedValue;
+
+    public Integer getOid() {
+        return oid;
+    }
+
+    public void setOid(Integer oid) {
+        this.oid = oid;
+    }
+
+    public String getEntitySource() {
+        return entitySource;
+    }
+
+    public void setEntitySource(String entitySource) {
+        this.entitySource = entitySource;
+    }
+
+    public Integer getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Integer creator) {
+        this.creator = creator;
+    }
+
+    public Integer getMdrMappingOid() {
+        return mdrMappingOid;
+    }
+
+    public void setMdrMappingOid(Integer mdrMappingOid) {
+        this.mdrMappingOid = mdrMappingOid;
+    }
+
+    public String getMdrPermittedValue() {
+        return mdrPermittedValue;
+    }
+
+    public void setMdrPermittedValue(String mdrPermittedValue) {
+        this.mdrPermittedValue = mdrPermittedValue;
+    }
+
+}
