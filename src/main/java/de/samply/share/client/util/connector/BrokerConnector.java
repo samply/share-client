525a22ffc0aa6f8eb3538d6800b4ef57c82be7c3
dolch <dolch.christoph@mh-hannover.de>
2018-10-23 15:50:29 +0200

    Anonymisation implemented in connector by adding random number and rounding up number of donors and samples plus testing

--- src/main/java/de/samply/share/client/util/connector/BrokerConnector.java
+++ src/main/java/de/samply/share/client/util/connector/BrokerConnector.java
@@ -83,7 +83,6 @@ import java.util.Map;
 
 import static de.samply.share.common.utils.Constants.AUTH_HEADER_VALUE_SAMPLY;
 
-import java.util.Random;
 /**
  * A connector that handles all communication with a searchbroker
  */
@@ -603,28 +602,9 @@ public class BrokerConnector {
                 }
             } else if (ldmConnector instanceof LdmConnectorSamplystoreBiobank) {
                 BbmriResult result = (BbmriResult) reply;
-                //stats.put("donor",result.getNumberOfDonors()); // if no disguise methode is used
-                //stats.put("sample",result.getNumberOfSamples());
-                int originalDonorNumber = result.getNumberOfDonors();
-                int originalSampleNumber = result.getNumberOfSamples();
-                int disguisedNumberOfDonors;
-                int disguisedNumberOfSamples;
-                Random rndmDonors = new Random();
-                Random rndmSamples = new Random();
-                if (originalDonorNumber > 0) {
-                    disguisedNumberOfDonors = 10 * ((originalDonorNumber + rndmDonors.nextInt(4) + 9) / 10); // disguise: add random number (0 to 3) & round up
-                } else {
-                    disguisedNumberOfDonors = 0;
-                }
 
-                if (originalSampleNumber > 0) {
-                    disguisedNumberOfSamples = 10 * ((originalSampleNumber + rndmSamples.nextInt(4) + 9) / 10); // disguise: add random number (0 to 3) & round up
-                } else {
-                    disguisedNumberOfSamples = 0;
-                }
-
-                stats.put("donor", disguisedNumberOfDonors);
-                stats.put("sample", disguisedNumberOfSamples);
+                stats.put("donor", NumberDisguiser.getDisguisedNumber(result.getNumberOfDonors()));
+                stats.put("sample", NumberDisguiser.getDisguisedNumber(result.getNumberOfSamples()));
                 replyString = stats.toString();
             }
             StringEntity entity = new StringEntity(replyString);
@@ -649,6 +629,7 @@ public class BrokerConnector {
             throw new BrokerConnectorException(e);
         }
     }
+
 
     /**
      * Set the last checked timestamp for this broker in the database
---
+++ src/test/java/de/samply/share/client/util/connector/NumberDisguiserTest.java
@@ -0,0 +1,59 @@
+package de.samply.share.client.util.connector;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(Parameterized.class)
+
+public class NumberDisguiserTest {
+
+    private int number;
+    private int lower;
+    private int upper;
+
+    public NumberDisguiserTest(int number, int lower, int upper) {
+
+        this.number = number;
+        this.lower = lower;
+        this.upper = upper;
+
+    }
+
+    @Parameterized.Parameters(name = "{index}: Result for {0} should be either {1} or {2}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {0, 0, 0},
+                {1, 10, 10},
+                {2, 10, 10},
+                {3, 10, 10},
+                {7, 10, 10},
+                {8, 10, 20},
+                {9, 10, 20},
+                {17, 20, 20},
+                {18, 20, 30},
+                {19, 20, 30},
+
+        });
+    }
+
+    @Test
+    public void testGetDisguisedNumber() {
+
+        List<Integer> possibleValues = new ArrayList<>();
+        possibleValues.add(lower);
+        possibleValues.add(upper);
+
+        assertThat(possibleValues, hasItems(NumberDisguiser.getDisguisedNumber(number)));
+
+    }
+}
