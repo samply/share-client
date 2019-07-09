fbe7d2eb3ab87e9b82f209f4978cfb1e5a054757
tasd <d.tas@dkfz-heidelberg.de>
2019-07-09 15:31:46 +0200

    delete boolean

--- src/main/java/de/samply/share/client/util/connector/LdmConnectorCql.java
+++ src/main/java/de/samply/share/client/util/connector/LdmConnectorCql.java
@@ -1,11 +1,17 @@
 package de.samply.share.client.util.connector;
 
+import com.google.common.base.Stopwatch;
+import de.samply.common.ldmclient.AbstractLdmClient;
 import de.samply.common.ldmclient.LdmClientException;
 import de.samply.common.ldmclient.cql.LdmClientCql;
+import de.samply.common.ldmclient.model.LdmQueryResult;
+import de.samply.share.client.model.EnumConfigurationTimings;
 import de.samply.share.client.model.check.ReferenceQueryCheckResult;
 import de.samply.share.client.util.connector.exception.LDMConnectorException;
-import de.samply.share.model.common.*;
+import de.samply.share.client.util.db.ConfigurationUtil;
+import de.samply.share.common.utils.SamplyShareUtils;
 import de.samply.share.model.common.Error;
+import de.samply.share.model.common.QueryResultStatistic;
 import de.samply.share.model.cql.CqlResult;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.logging.log4j.LogManager;
@@ -15,13 +21,18 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import java.io.File;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class LdmConnectorCql extends AbstractLdmConnector<LdmClientCql, String, CqlResult, QueryResultStatistic, Error> {
 
     private static final Logger logger = LogManager.getLogger(LdmConnectorCql.class);
 
-    LdmConnectorCql(boolean useCaching) {
+    public LdmConnectorCql(boolean useCaching) {
         super(useCaching);
+    }
+
+    public LdmConnectorCql(boolean useCaching, int maxCacheSize) {
+        super(useCaching, maxCacheSize);
     }
 
     @Override
@@ -31,12 +42,12 @@ public class LdmConnectorCql extends AbstractLdmConnector<LdmClientCql, String, 
 
     @Override
     LdmClientCql createLdmClient(CloseableHttpClient httpClient, String baseUrl, boolean useCaching) throws LdmClientException {
-        return new LdmClientCql(httpClient,baseUrl);
+        return new LdmClientCql(httpClient, baseUrl);
     }
 
     @Override
     LdmClientCql createLdmClient(CloseableHttpClient httpClient, String baseUrl, boolean useCaching, int maxCacheSize) throws LdmClientException {
-        return createLdmClient(httpClient,baseUrl,useCaching);
+        return createLdmClient(httpClient, baseUrl, useCaching);
     }
 
     @Override
@@ -57,13 +68,12 @@ public class LdmConnectorCql extends AbstractLdmConnector<LdmClientCql, String, 
 
     @Override
     public String postQuery(String query, List<String> removeKeysFromView, boolean completeMdsViewFields, boolean statisticsOnly, boolean includeAdditionalViewfields) throws LDMConnectorException {
-            try {
-                return ldmClient.postQuery(query, statisticsOnly);
-            } catch (LdmClientException e) {
-                throw new LDMConnectorException(e);
-            }
+        try {
+            return ldmClient.postQuery(query, statisticsOnly);
+        } catch (LdmClientException e) {
+            throw new LDMConnectorException(e);
         }
-
+    }
 
     @Override
     public CqlResult getResultsFromPage(String location, int page) throws LDMConnectorException {
@@ -77,21 +87,106 @@ public class LdmConnectorCql extends AbstractLdmConnector<LdmClientCql, String, 
         return false;
     }
 
-    @Override
-    public boolean isResultDone(String location, QueryResultStatistic qrs) throws LDMConnectorException {
-        // TODO: Check if we need this when not using pages and views
-        return false;
+
+    public boolean isResultDone(String location, QueryResultStatistic queryResultStatistic) throws LDMConnectorException {
+        if (SamplyShareUtils.isNullOrEmpty(location)) {
+            throw new LDMConnectorException("Location of query is empty");
+        }
+        if (queryResultStatistic != null) {
+            if (queryResultStatistic.getTotalSize() >= 0) {
+                return true;
+            }
+            return false;
+        } else {
+            throw new LDMConnectorException("QueryResultStatistic is null.");
+        }
     }
 
     @Override
     public int getPatientCount(boolean dktkFlagged) throws LDMConnectorException, InterruptedException {
-        // TODO: Implement
+        int maxAttempts = ConfigurationUtil.getConfigurationTimingsElementValue(
+                EnumConfigurationTimings.JOB_CHECK_INQUIRY_STATUS_RESULTS_RETRY_ATTEMPTS);
+        int secondsSleep = ConfigurationUtil.getConfigurationTimingsElementValue(
+                EnumConfigurationTimings.JOB_CHECK_INQUIRY_STATUS_RESULTS_RETRY_INTERVAL_SECONDS);
+        int retryNr = 0;
+
+        String query = createQueryForMonitoring();
+        String resultLocation;
+        try {
+            resultLocation = ldmClient.postQuery(query, true);
+        } catch (LdmClientException e) {
+            throw new LDMConnectorException(e);
+        }
+        do {
+            try {
+                Integer resultCount = getResultCount(resultLocation);
+                if (resultCount != null) {
+                    return resultCount;
+                }
+            } catch (LDMConnectorException e) {
+                // Catch the exception since it might just mean the result is not ready yet
+            }
+            TimeUnit.SECONDS.sleep(secondsSleep);
+        } while (++retryNr < maxAttempts);
         return 0;
     }
 
     @Override
     public ReferenceQueryCheckResult getReferenceQueryCheckResult(String referenceQuery) throws LDMConnectorException {
-        // TODO: Check if we must implent this method
-        return null;
+        ReferenceQueryCheckResult result = new ReferenceQueryCheckResult();
+        try {
+            Stopwatch stopwatch = Stopwatch.createStarted();
+            String resultLocation = ldmClient.postQuery(referenceQuery, false);
+
+            int maxAttempts = ConfigurationUtil.getConfigurationTimingsElementValue(
+                    EnumConfigurationTimings.JOB_CHECK_INQUIRY_STATUS_RESULTS_RETRY_ATTEMPTS);
+            int secondsSleep = ConfigurationUtil.getConfigurationTimingsElementValue(
+                    EnumConfigurationTimings.JOB_CHECK_INQUIRY_STATUS_RESULTS_RETRY_INTERVAL_SECONDS);
+            int retryNr = 0;
+            do {
+                try {
+                    LdmQueryResult ldmQueryResult = ldmClient.getStatsOrError(resultLocation);
+
+                    if (ldmQueryResult.hasError()) {
+                        stopwatch.reset();
+                        Error error = ldmQueryResult.getError();
+
+                        switch (error.getErrorCode()) {
+                            case AbstractLdmClient.ERROR_CODE_DATE_PARSING_ERROR:
+                            case AbstractLdmClient.ERROR_CODE_UNIMPLEMENTED:
+                            case AbstractLdmClient.ERROR_CODE_UNCLASSIFIED_WITH_STACKTRACE:
+                                getLogger().warn("Could not execute reference query correctly. Error: " + error.getErrorCode() + ": " + error.getDescription());
+                                return result;
+                            case AbstractLdmClient.ERROR_CODE_UNKNOWN_MDRKEYS:
+                            default:
+                                stopwatch.start();
+                                resultLocation = ldmClient.postQuery(referenceQuery, false);
+                                break;
+                        }
+                    } else if (ldmQueryResult.hasResult()) {
+                        QueryResultStatistic qrs = ldmQueryResult.getResult();
+                        result.setCount(qrs.getTotalSize());
+                        if (isResultDone(resultLocation, qrs)) {
+                            stopwatch.stop();
+                            result.setExecutionTimeMilis(stopwatch.elapsed(TimeUnit.MILLISECONDS));
+                            return result;
+                        }
+                    }
+
+                    retryNr += 1;
+                    TimeUnit.SECONDS.sleep(secondsSleep);
+                } catch (InterruptedException e) {
+                    return result;
+                }
+            } while (retryNr < maxAttempts);
+        } catch (LdmClientException e) {
+            throw new LDMConnectorException(e);
+        }
+        return result;
     }
+
+    private String createQueryForMonitoring() {
+        return "";
+    }
+
 }
