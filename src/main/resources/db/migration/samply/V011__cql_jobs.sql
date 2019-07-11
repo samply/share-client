37c49eb0e3c2ffa2856b623fe62b09ca3d35837c
Björn Kroll <Bjoern.Kroll@itcr.uni-luebeck.de>
2019-07-11 19:17:20 +0200

    Split CheckInquiryStatusJob and ExecuteInquiryJob into subclasses for DKTK, CQL and Samplystore (bugfix quartz flyway)

--- src/main/resources/db/migration/samply/V011__cql_jobs.sql
+++ src/main/resources/db/migration/samply/V011__cql_jobs.sql
@@ -1,12 +1,12 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
 -- Set the cron expressions for some of the jobs
-INSERT INTO samply.job_schedule(job_key, cron_expression, paused) VALUES ('DecentralSearchGroup.CollectInquiriesJobCentraxx', '0/10 * * * * ?', false);
-INSERT INTO samply.job_schedule(job_key, cron_expression, paused) VALUES ('DecentralSearchGroup.CollectInquiriesJobCql', '0/10 * * * * ?', false);
-INSERT INTO samply.job_schedule(job_key, cron_expression, paused) VALUES ('DecentralSearchGroup.CollectInquiriesJobView', '0/10 * * * * ?', false);
-INSERT INTO samply.job_schedule(job_key, cron_expression, paused) VALUES ('DecentralSearchGroup.ExecuteInquiriesJobCentraxx', '0/5 * * * * ?', false);
-INSERT INTO samply.job_schedule(job_key, cron_expression, paused) VALUES ('DecentralSearchGroup.ExecuteInquiriesJobCql', '0/5 * * * * ?', false);
-INSERT INTO samply.job_schedule(job_key, cron_expression, paused) VALUES ('DecentralSearchGroup.ExecuteInquiriesJobView', '0/5 * * * * ?', false);
+INSERT INTO samply.job_schedule(job_key, cron_expression, paused) VALUES ('InquiryGroup.CheckInquiryStatusJobCentraxx', '0/10 * * * * ?', false);
+INSERT INTO samply.job_schedule(job_key, cron_expression, paused) VALUES ('InquiryGroup.CheckInquiryStatusJobSamplystoreBiobanks', '0/10 * * * * ?', false);
+INSERT INTO samply.job_schedule(job_key, cron_expression, paused) VALUES ('InquiryGroup.CheckInquiryStatusJobCql', '0/10 * * * * ?', false);
+INSERT INTO samply.job_schedule(job_key, cron_expression, paused) VALUES ('InquiryGroup.ExecuteInquiryJobCentraxx', '0/5 * * * * ?', false);
+INSERT INTO samply.job_schedule(job_key, cron_expression, paused) VALUES ('InquiryGroup.ExecuteInquiryJobSamplystoreBiobanks', '0/5 * * * * ?', false);
+INSERT INTO samply.job_schedule(job_key, cron_expression, paused) VALUES ('InquiryGroup.ExecuteInquiryJobCql', '0/5 * * * * ?', false);
 
-DELETE FROM samply.job_schedule WHERE job_key = 'DecentralSearchGroup.CollectInquiriesJob';
-DELETE FROM samply.job_schedule WHERE job_key = 'DecentralSearchGroup.ExecuteInquiriesJob';
+DELETE FROM samply.job_schedule WHERE job_key = 'InquiryGroup.CheckInquiryStatusJob';
+DELETE FROM samply.job_schedule WHERE job_key = 'InquiryGroup.ExecuteInquiryJob';
