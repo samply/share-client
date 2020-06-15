23e5919b220513d671deb4a21e89544b302a4ead
Deniz Tas <d.tas@dkfz-heidelberg.de>
2020-06-15 11:19:54 +0200

    added NotImplementedException

--- src/main/java/de/samply/share/client/feature/EmptyStateRepository.java
+++ src/main/java/de/samply/share/client/feature/EmptyStateRepository.java
@@ -3,15 +3,16 @@ package de.samply.share.client.feature;
 import org.togglz.core.Feature;
 import org.togglz.core.repository.FeatureState;
 import org.togglz.core.repository.StateRepository;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class EmptyStateRepository implements StateRepository {
     @Override
     public FeatureState getFeatureState(Feature feature) {
-        return null;
+        throw new NotImplementedException();
     }
 
     @Override
     public void setFeatureState(FeatureState featureState) {
-
+        throw new NotImplementedException();
     }
 }
