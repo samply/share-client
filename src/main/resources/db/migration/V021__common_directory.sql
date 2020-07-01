8d97929fc49b74a5ed467c01948cce339d8bbb94
Deniz Tas <d.tas@dkfz-heidelberg.de>
2020-07-01 08:42:06 +0200

    renamed SQL file

--- src/main/resources/db/migration/V021__common_directory.sql
+++ src/main/resources/db/migration/V021__common_directory.sql
@@ -1,3 +1,1 @@
---INSERT INTO pg_enum (enumtypid, enumlabel, enumsortorder)
---    SELECT 'target_type'::regtype::oid, 'TT_DIRECTORY', ( SELECT MAX(enumsortorder) + 1 FROM pg_enum WHERE enumtypid = 'target_type'::regtype );
 ALTER TYPE target_type ADD VALUE 'TT_DIRECTORY';
