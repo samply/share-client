763ba850aea3ebb6e4797ada79cf790e67cfc39d
Deniz Tas <d.tas@dkfz-heidelberg.de>
2020-09-09 16:05:21 +0200

    abstract 5 minutes from LocalDateTime

--- src/main/java/de/samply/share/client/job/CheckScheduledInquiriesJob.java
+++ src/main/java/de/samply/share/client/job/CheckScheduledInquiriesJob.java
@@ -1,23 +1,26 @@
 package de.samply.share.client.job;
 
+import de.samply.share.client.control.ApplicationBean;
+import de.samply.share.client.job.params.ExecuteInquiryJobParams;
 import de.samply.share.client.model.EnumConfigurationTimings;
 import de.samply.share.client.model.db.enums.InquiryCriteriaStatusType;
 import de.samply.share.client.model.db.enums.InquiryStatusType;
-import de.samply.share.client.model.db.tables.pojos.Broker;
 import de.samply.share.client.model.db.tables.pojos.Inquiry;
 import de.samply.share.client.model.db.tables.pojos.InquiryCriteria;
 import de.samply.share.client.model.db.tables.pojos.InquiryDetails;
-import de.samply.share.client.util.connector.BrokerConnector;
-import de.samply.share.client.util.connector.exception.BrokerConnectorException;
-import de.samply.share.client.util.db.*;
+import de.samply.share.client.util.db.ConfigurationUtil;
+import de.samply.share.client.util.db.InquiryCriteriaUtil;
+import de.samply.share.client.util.db.InquiryDetailsUtil;
+import de.samply.share.client.util.db.InquiryUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.SchedulerException;
 
 import java.sql.Timestamp;
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -37,36 +40,19 @@ public class CheckScheduledInquiriesJob implements Job {
      * @throws JobExecutionException
      */
     private void checkProcessingInquiry() throws JobExecutionException {
-        List<InquiryDetails> inquiryDetailsList = InquiryDetailsUtil.getInquiryDetailsByStatus(InquiryStatusType.IS_PROCESSING);
-        if (inquiryDetailsList.isEmpty()) {
-            return;
-        }
-        for (InquiryDetails inquiryDetails : inquiryDetailsList) {
-            if (checkScheduledAt(inquiryDetails)) {
-                StringBuilder stringBuilder = new StringBuilder();
-                abandonedInquiryCriteria(inquiryDetails);
-                abandonedInquiryDetails(inquiryDetails);
-                Inquiry inquiry = InquiryUtil.fetchInquiryById(inquiryDetails.getInquiryId());
-                stringBuilder.append("Inquiry with ID ").append(inquiry.getId()).append("was abandoned because the execution time was too long. ");
-                logger.error(stringBuilder.toString());
-                sendErrorMessageToBroker(inquiry, stringBuilder.toString());
-            }
-        }
-    }
-
-    /**
-     * Send the error message to the broker
-     *
-     * @param inquiry the inquiry where the error was found
-     * @param error   the error message
-     * @throws JobExecutionException
-     */
-    private void sendErrorMessageToBroker(Inquiry inquiry, String error) throws JobExecutionException {
-        Broker broker = BrokerUtil.fetchBrokerById(inquiry.getBrokerId());
-        BrokerConnector brokerConnector = new BrokerConnector(broker);
         try {
-            brokerConnector.sendErrorMessage(error);
-        } catch (BrokerConnectorException e) {
+            List<InquiryDetails> inquiryDetailsList = InquiryDetailsUtil.getInquiryDetailsByStatus(InquiryStatusType.IS_PROCESSING);
+            for (InquiryDetails inquiryDetails : inquiryDetailsList) {
+                if (checkScheduledAt(inquiryDetails)) {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    abandonedInquiryCriteria(inquiryDetails);
+                    abandonedInquiryDetails(inquiryDetails);
+                    Inquiry inquiry = InquiryUtil.fetchInquiryById(inquiryDetails.getInquiryId());
+                    stringBuilder.append("Inquiry with ID ").append(inquiry.getId()).append("was abandoned because the execution time was too long. ");
+                    logger.warn(stringBuilder.toString());
+                }
+            }
+        } catch (SchedulerException e) {
             throw new JobExecutionException(e);
         }
     }
@@ -76,10 +62,25 @@ public class CheckScheduledInquiriesJob implements Job {
      *
      * @param inquiryDetails the inquiry detail which status has to been changed
      */
-    private void abandonedInquiryDetails(InquiryDetails inquiryDetails) {
+    private void abandonedInquiryDetails(InquiryDetails inquiryDetails) throws SchedulerException {
         InquiryDetails inquiryDetailsTmp = InquiryDetailsUtil.fetchInquiryDetailsById(inquiryDetails.getId());
         inquiryDetailsTmp.setStatus(InquiryStatusType.IS_ABANDONED);
         InquiryDetailsUtil.updateInquiryDetails(inquiryDetailsTmp);
+        unscheduleJob(inquiryDetails.getInquiryId());
+    }
+
+    /**
+     * unschedule the inquiry execution job
+     *
+     * @param inquiryId the inquiry whose job should be unscheduled
+     * @throws SchedulerException
+     */
+    private void unscheduleJob(int inquiryId) throws SchedulerException {
+        for (JobExecutionContext jobExecutionContext : ApplicationBean.getScheduler().getCurrentlyExecutingJobs()) {
+            if (jobExecutionContext.getMergedJobDataMap().get(ExecuteInquiryJobParams.INQUIRY_ID).equals(inquiryId)) {
+                ApplicationBean.getScheduler().unscheduleJob(jobExecutionContext.getRecoveringTriggerKey());
+            }
+        }
     }
 
     /**
@@ -104,7 +105,8 @@ public class CheckScheduledInquiriesJob implements Job {
     private boolean checkScheduledAt(InquiryDetails inquiryDetails) {
         int minutes = ConfigurationUtil.getConfigurationTimingsElement(EnumConfigurationTimings.JOB_CHECK_SCHEDULED_INQUIRY_INTERVAL_MINUTES).getSetting();
         Timestamp scheduledAt = inquiryDetails.getScheduledAt();
-        Timestamp maxTime = new Timestamp(new Date().getTime() - TimeUnit.MINUTES.toMillis(minutes));
+        LocalDateTime fiveMinutesAgo = LocalDateTime.now().minusMinutes(TimeUnit.MINUTES.toMillis(minutes));
+        Timestamp maxTime = Timestamp.valueOf(fiveMinutesAgo);
         return maxTime.after(scheduledAt);
     }
 
