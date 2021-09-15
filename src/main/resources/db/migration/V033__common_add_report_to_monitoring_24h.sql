69f9c0fd842a034f1f60fcc87ecd0443f9bd6352
Marcel Kissel <marcel.kissel@dkfz-heidelberg.de>
2021-09-15 14:28:07 +0200

    Changed visibility of ReportToMonitoringJobLongFrequence to dktk only

--- src/main/resources/db/migration/V033__common_add_report_to_monitoring_24h.sql
+++ src/main/resources/db/migration/V033__common_add_report_to_monitoring_24h.sql
@@ -1,1 +1,1 @@
-INSERT INTO samply.job_schedule(job_key, cron_expression, paused) VALUES ('MaintenanceGroup.ReportToMonitoringJobLongFrequence', '0 0 3 ? * * *', false);
+INSERT INTO samply.job_schedule(job_key, cron_expression, paused) VALUES ('CentralSearchGroup.ReportToMonitoringJobLongFrequence', '0 0 3 ? * * *', false);
