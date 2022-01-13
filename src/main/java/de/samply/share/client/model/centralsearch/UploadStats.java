4519ec7ce23aed3581c68033f9a613ee30632ad8
juarez <d.juarez@dkfz-heidelberg.de>
2022-01-13 11:18:12 +0100

    add jackson xml annotations

--- src/main/java/de/samply/share/client/model/centralsearch/UploadStats.java
+++ src/main/java/de/samply/share/client/model/centralsearch/UploadStats.java
@@ -1,13 +1,21 @@
 package de.samply.share.client.model.centralsearch;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
 /**
  * Represents the upload stats as received from central search.
  */
 
+@JacksonXmlRootElement(localName = "UploadStats")
 public class UploadStats {
 
-
+  @JacksonXmlProperty(localName = "LastUploadTimestamp")
   private String lastUploadTimestamp;
+
+  public UploadStats() {
+    super();
+  }
 
   public UploadStats(String lastUploadTimestamp) {
     this.lastUploadTimestamp = lastUploadTimestamp;
@@ -15,6 +23,10 @@ public class UploadStats {
 
   public String getLastUploadTimestamp() {
     return lastUploadTimestamp;
+  }
+
+  public void setLastUploadTimestamp(String lastUploadTimestamp) {
+    this.lastUploadTimestamp = lastUploadTimestamp;
   }
 
 }
